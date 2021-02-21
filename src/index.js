88afbc74452af5c23713785199fd95fb73cae61b
Finsoy <vadimka.cry.00@gmail.com>
2021-02-21 18:12:55 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  
-}
+    // your solution
+    let stack = [];
+
+    str.split("");
+    for (let iterator of str) {
+        let bracketsConfigIndex = bracketsConfig.indexOf(iterator);
+
+        if (bracketsConfigIndex % 2 === 0) {
+            stack.push(bracketsConfigIndex + 1);
+        } else {
+            if (stack.pop() !== bracketsConfigIndex) {
+                return false;
+            }
+        }
+
+        if (stack.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
